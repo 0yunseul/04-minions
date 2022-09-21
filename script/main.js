e9f1edb6ea351fa40ac58ee141d5999f0b2d5f2f
jooyoung <jooyoung.cairo@gmail.com>
2022-09-21 10:06:26 +0900

    javascript version
    jquery version을 js버전으로 변경

---
+++ script/main.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", function(){
+			setTimeout(showAfterLoad, 3000);
+			const POPUP = document.querySelector("#popup");
+			const BODY = document.querySelector("body");
+			const CLOSE = document.querySelector("#close");
+					
+			function showAfterLoad() { 
+				POPUP.style.display = "block";
+				BODY.style.overflow = "hidden";					
+				}
+					
+			CLOSE.addEventListener("click", function(){
+				POPUP.style.display = "none";
+				BODY.style.overflow = "auto";	
+			})
+					
+})
