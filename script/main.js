089faff7e8257143e7adfb5ef13b4e443c8be7d8
jooyoung <jooyoung.cairo@gmail.com>
2022-09-21 14:12:25 +0900

    carousel script finish
    미니언즈2 review 캐러셀 적용

--- script/main.js
+++ script/main.js
@@ -1,7 +1,8 @@
 window.addEventListener("DOMContentLoaded", function(){
-			setTimeout(showAfterLoad, 3000);
+			//setTimeout(showAfterLoad, 3000);
+			const POSTS = document.querySelector("#posts");
 			const POPUP = document.querySelector("#popup");
-			const BODY = document.querySelector("body");
+			const BODY  = document.querySelector("body");
 			const CLOSE = document.querySelector("#close");
 					
 			function showAfterLoad() { 
@@ -13,5 +14,23 @@ window.addEventListener("DOMContentLoaded", function(){
 				POPUP.style.display = "none";
 				BODY.style.overflow = "auto";	
 			})
-					
+			const NEXT = document.querySelector(".next");
+			const PREV = document.querySelector(".prev");
+			let postIndex = 0;		
+				
+			NEXT.addEventListener("click",function(){
+				postIndex++;
+				if(postIndex>=3) postIndex = 0;
+				let coords = -920 * postIndex;
+				POSTS.style.marginLeft = coords+"px";
+			})
+			PREV.addEventListener("click",function(){
+				postIndex--;
+				if(postIndex<0 ) postIndex = 2;
+				let coords = -920 * postIndex;
+				POSTS.style.marginLeft = coords+"px";
+			})
+			
+			
+			
 })
